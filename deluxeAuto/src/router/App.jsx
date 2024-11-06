4fcbd3b5b2eaca0b9decae06b6f53487dc94ee33
Gabriel Lima <gabrilima2004@gmail.com>
2024-11-06 15:09:43 -0300

    Esquematização das rotas

---
+++ deluxeAuto/src/router/App.jsx
@@ -0,0 +1,44 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" Component={}/>
+
+        <Route path="/worldmap" Component={}/>
+
+        <Route path="/worldmap/italia" Component={}/>
+
+        <Route path="/worldmap/alemanha" Component={}/>
+
+        <Route path="/worldmap/franca" Component={}/>
+
+        <Route path="/worldmap/suecia" Component={}/>
+
+        <Route path="/worldmap/inglaterra" Component={}/>
+
+        <Route path="/worldmap/japao" Component={}/>
+
+        <Route path="/worldmap/eua" Component={}/>
+
+        <Route path="/conta" Component={}/>
+
+        <Route path="/sobre-nos" Component={}/>
+
+        <Route path="/diamond" Component={}/>
+
+        <Route path="/diamond/classics" Component={}/>
+
+        <Route path="/diamond/race" Component={}/>
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
