52154583972d01205883d13284f72a1789dcff64
Gabriel Lima <gabrilima2004@gmail.com>
2024-11-10 12:53:22 -0300

    putaquepariu

--- deluxeAuto/src/components/Carros/Italia/index.jsx
+++ deluxeAuto/src/components/Carros/Italia/index.jsx
@@ -1,4 +1,8 @@
 import "./styleIT.css"
+import LogoFerrari from "../../../assets/logos/ferrari.png"
+import LogoLambo from "../../../assets/logos/Lamborghini.jpg"
+import Roma from "../../../assets/images/normal/Roma.webp"
+import Urus from "../../../assets/images/normal/Urus.jpeg"
 
 export default function CarrosItalia(){
     return (
@@ -9,13 +13,13 @@ export default function CarrosItalia(){
                 <img src="https://img.icons8.com/?size=100&id=BV2gY6QOLTF9&format=png&color=000000" alt="" />
             </div>
 
-            <div className="Italia-Fileira1">
+            <div className="Fileira">
 
-                    <div class="Ferrari-Roma-Card">
-                        <img className="fotoprincipal" src="Fotos/Roma.webp" alt="Ferrari Roma"/>
-                        <div className="Roma-Container">
-                            <h4 className="logo-nome" ><img src="Logos/Ferrari.png" alt="Ferrari Logo"/> Ferrari Roma</h4>
-                            <div clasSName="Roma-Info">
+                    <div class="Card-carro">
+                        <img className="fotoprincipal" src={Roma} alt="Ferrari Roma"/>
+                        <div className="Container">
+                            <h4 className="logo-nome" ><img src={LogoFerrari} alt=""/> Ferrari Roma</h4>
+                            <div className="Info">
                                 <p><img src="https://img.icons8.com/?size=100&id=zPV5akQ5KlKf&format=png&color=000000" alt="Motor" width="16px"/>620cv | 3.9L V8</p>
                                 <p><img src="Icones/shifter.png" alt="Câmbio"/>AT 8 Marchas</p>
                                 <p><img src="https://img.icons8.com/?size=100&id=25945&format=png&color=000000" width="16px" alt="Paleta Cor"/>Rosso Corsa</p>
@@ -24,11 +28,11 @@ export default function CarrosItalia(){
                         </div>
                     </div>
 
-                    <div class="Lamborghini-Urus-Card">
-                        <img className="fotoprincipal" src="Fotos/Urus.jpeg"  alt="Lamborghini Urus S"/>
-                        <div className="Urus-Container">
-                            <h4 className="logo-nome" ><img src="Logos/Lamborghini.jpg" alt="Lamborghini Logo"/>Lamborghini Urus S</h4>
-                            <div className="Urus-Info">
+                    <div class="Card-carro">
+                        <img className="fotoprincipal" src={Urus}  alt="Lamborghini Urus S"/>
+                        <div className="Container">
+                            <h4 className="logo-nome" ><img src={LogoLambo} alt=""/>Lamborghini Urus S</h4>
+                            <div className="Info">
                                 <p><img src="https://img.icons8.com/?size=100&id=zPV5akQ5KlKf&format=png&color=000000" alt="Motor" width="16px"/>666cv | 4.0L V8</p>
                                 <p><img src="Icones/shifter.png" alt="Câmbio"/>AT|MT 8 Marchas</p>
                                 <p><img src="https://img.icons8.com/?size=100&id=25945&format=png&color=000000" width="16px" alt="Paleta Cor"/>Grigio Telesto</p>
