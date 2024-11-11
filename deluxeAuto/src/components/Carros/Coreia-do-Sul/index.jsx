e2b39773c75e30e4d790b3c254fb84067e152edb
Gabriel Lima <gabrilima2004@gmail.com>
2024-11-11 10:43:07 -0300

    EU SOU MUITO BURRO PUTA QUE PARIU, pelo menos resolvi o problema

--- deluxeAuto/src/components/Carros/Coreia-do-Sul/index.jsx
+++ deluxeAuto/src/components/Carros/Coreia-do-Sul/index.jsx
@@ -8,40 +8,40 @@ import Ioniq from "../../../assets/images/normal/IONIQ5.jpg"
 export default function CarrosCoreia(){
     return(
         <>
-        <section id="Showroom">
-            <div className="paisBandeira">
-                    <h2>Coréia do Sul</h2>
-                    <img src="https://img.icons8.com/?size=100&id=33288&format=png&color=000000" alt="" />
+            <section id="Showroom">
+                <div className="paisBandeira">
+                        <h2>Coréia do Sul</h2>
+                        <img src="https://img.icons8.com/?size=100&id=33288&format=png&color=000000" alt="" />
                 </div>
 
-                <div className="Fileira">
-                        <div class="Card-carro">
-                            <img className="fotoprincipal" src={Palisade} alt="Hyundai Palisade"/>
-                            <div className="Container">
-                                <h4 className="logo-nome" ><img src={LogoHyundai} alt=""/>Hyundai Palisade Signature</h4>
-                                <div className="Info">
-                                    <p><img src="https://img.icons8.com/?size=100&id=zPV5akQ5KlKf&format=png&color=000000" alt="Motor" width="16px"/>295cv | 3.8L V6</p>
-                                    <p><img src={Marcha} alt="Câmbio"/>AT 8 Marchas</p>
-                                    <p><img src="https://img.icons8.com/?size=100&id=25945&format=png&color=000000" width="16px" alt="Paleta Cor"/>Moonlight Pearl Blue</p>
+                    <div className="Fileira">
+                            <div class="Card-carro">
+                                <img className="fotoprincipal" src={Palisade} alt="Hyundai Palisade"/>
+                                <div className="Container">
+                                    <h4 className="logo-nome" ><img src={LogoHyundai} alt=""/>Hyundai Palisade Signature</h4>
+                                    <div className="Info">
+                                        <p><img src="https://img.icons8.com/?size=100&id=zPV5akQ5KlKf&format=png&color=000000" alt="Motor" width="16px"/>295cv | 3.8L V6</p>
+                                        <p><img src={Marcha} alt="Câmbio"/>AT 8 Marchas</p>
+                                        <p><img src="https://img.icons8.com/?size=100&id=25945&format=png&color=000000" width="16px" alt="Paleta Cor"/>Moonlight Pearl Blue</p>
+                                    </div>
+                                    <h4 class="preco">R$ 450.000,00</h4>
                                 </div>
-                                <h4 class="preco">R$ 450.000,00</h4>
                             </div>
-                        </div>
 
-                        <div class="Card-carro">
-                            <img className="fotoprincipal" src={Ioniq}  alt="Hyundai IONIQ5"/>
-                            <div className="Container">
-                                <h4 className="logo-nome" ><img src={LogoLambo} alt=""/>Hyundai IONIQ5 Signature</h4>
-                                <div className="Info">
-                                    <p><img src="https://img.icons8.com/?size=100&id=zPV5akQ5KlKf&format=png&color=000000" alt="Motor" width="16px"/>84Kwh (365cv) | Elétrico</p>
-                                    <p><img src={Marcha} alt="Câmbio"/>AT</p>
-                                    <p><img src="https://img.icons8.com/?size=100&id=25945&format=png&color=000000" width="16px" alt="Paleta Cor"/>Titan Gray</p>
+                            <div class="Card-carro">
+                                <img className="fotoprincipal" src={Ioniq}  alt="Hyundai IONIQ5"/>
+                                <div className="Container">
+                                    <h4 className="logo-nome" ><img src={LogoHyundai} alt=""/>Hyundai IONIQ5 Signature</h4>
+                                    <div className="Info">
+                                        <p><img src="https://img.icons8.com/?size=100&id=zPV5akQ5KlKf&format=png&color=000000" alt="Motor" width="16px"/>84Kwh - 365cv | Elétrico</p>
+                                        <p><img src={Marcha} alt="Câmbio"/>AT</p>
+                                        <p><img src="https://img.icons8.com/?size=100&id=25945&format=png&color=000000" width="16px" alt="Paleta Cor"/>Titan Gray</p>
+                                    </div>
+                                    <h4 class="preco">R$ 394.990,00</h4>
                                 </div>
-                                <h4 class="preco">R$ 394.990,00</h4>
                             </div>
-                        </div>
-                </div>
-        </section>
+                    </div>
+            </section>
         </>
     )
 }
--- deluxeAuto/src/views/Showrooms/Coreia-do-Sul/index.jsx
+++ deluxeAuto/src/views/Showrooms/Coreia-do-Sul/index.jsx
@@ -1,5 +1,5 @@
-import CarrosCoreia from "../../../components/Carros/Coreia-do-Sul";
 import Header from "../../../components/Header";
+import CarrosCoreia from "../../../components/Carros/Coreia-do-Sul";
 import Footer from "../../../components/Footer";
 
 
